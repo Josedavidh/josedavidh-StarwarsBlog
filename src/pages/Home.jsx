6531b68c31f93ef9547cf9de2b476f6e3bc19a5f
JoseDHernandez <168580718+Josedavidh@users.noreply.github.com>
2025-05-18 21:20:12 +0000

    improving

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,41 +4,121 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
 
-	const { store, dispatch } = useGlobalReducer()
-	const { characters, planets } = store
-	const { }
+	const { store, dispatch } = useGlobalReducer();
+	const { characters, planets } = store;
+
 
 	return (
 		<div className="container">
-			<h1 className="text-danger">Characters</h1>
-			<div className="the_carousel">
-				{
+			<div>
+				<h1 className="text-danger">Characters</h1>
+				<div className="the_carousel">
+					{
+						characters.map((char) => {
+							const { properties, _id, image } = char
 
-					store.characters.map(() => (
-						<div className="card card_shape" >
-							<img
-								src="https://i.pravatar.cc/250"
-								className="card-img-top"
-								alt="Imagen de ejemplo"
-							/>
-							<div className="card-body">
-								<h5 className="card-title">Character</h5>
-								<p className="card-text">
-									Gender: male<br></br>
-									Hair Color: blond<br></br>
-									Eye-Color: blue
+							return (
+								<div className="card card_shape" key={_id}>
+									<img
+										src={image}
+										className="card-img-top w-100"
+										alt={properties.name}
+									/>
+									<div className="card-body">
+										<h5 className="card-title">{properties?.name}</h5>
+										<p className="card-text">
+											Gender: {properties?.gender}
+										</p>
+										<p className="card-text">
+											Hair Color: {properties?.hair_color}
+										</p>
+										<p className="card-text">
+											Eye-Color: {properties?.eye_color}
+										</p>
+										<div className="card-footer-buttons">
 
-								</p>
-								<a href="#" className="btn btn-outline-primary">Learn more!</a>
-							</div>
-						</div>
-					))
-				}
+											<Link
+												to={`/character/${_id}`}
+												className="btn btn-outline-primary">
+												Learn more!
+											</Link>
+											<button className="btn btn-outline-warning"
+												onClick={() => {
 
+													dispatch({
+														type: "TOGGLE_FAVORITE",
+														payload: char
+													})
+												}}>
+												{store.favorites.some((fav) => fav._id == _id) ?
+													<i className="fas fa-heart heart-icon"></i> :
+													<i className="far fa-heart heart-icon"></i>
+												}
 
+											</button>
+										</div>
+									</div>
+								</div>
+							)
+						})
 
+					}
+				</div>
+			</div >
 
-			</div>
-		</div >
+			<div className="container mt-4">
+				<h1 className="text-danger mb-3">Planets</h1>
+				<div className="the_carousel">
+					{
+						planets.map((planet) => {
+							const { properties, _id, image } = planet;
+
+							return (
+								<div className="card card_shape" key={_id}>
+									<img
+										src={image}
+										className="card-img-top w-100"
+										alt={properties.name}
+									/>
+									<div className="card-body">
+										<h5 className="card-title">{properties?.name}</h5>
+										<p className="card-text">
+											Population: {properties?.population}
+										</p>
+										<p className="card-text">
+											Terrain: {properties?.terrain}
+										</p>
+										<div className="card-footer-buttons">
+
+											<Link
+												to={`/character/${_id}`}
+												className="btn btn-outline-primary">
+												Learn more!
+											</Link>
+											<button className="btn btn-outline-warning"
+												onClick={() => {
+
+													dispatch({
+														type: "TOGGLE_FAVORITE",
+														payload: planets
+													})
+												}}>
+												{store.favorites.some((fav) => fav._id == _id) ?
+													<i className="fas fa-heart heart-icon"></i> :
+													<i className="far fa-heart heart-icon"></i>
+												}
+
+											</button>
+										</div>
+									</div>
+								</div>
+							)
+						})
+
+					}
+				</div>
+			</div >
+		</div>
+
 	);
 }; 
